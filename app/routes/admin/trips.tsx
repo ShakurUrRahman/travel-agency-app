b15a05c32847ebd2dc210fc32b91a68610f85e26
ShakurUrRahman <shakururrahman@gmail.com>
2025-06-22 15:23:53 +0600

    Create trip

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,43 +1,43 @@
-// import { Header, TripCard } from "../../../components";
-// import { type LoaderFunctionArgs, useSearchParams } from "react-router";
-// import { getAllTrips, getTripById } from "~/appwrite/trips";
-// import { parseTripData } from "~/lib/utils";
-// import type { Route } from "./+types/trips";
+import { Header, TripCard } from "components";
+import { useSearchParams, type LoaderFunctionArgs } from "react-router";
+import { getAllTrips } from "~/appwrite/trips";
+import { parseTripData } from "~/lib/utils";
+import type { Route } from "./+types/trips";
+import { useState } from "react";
+import { PagerComponent } from "@syncfusion/ej2-react-grids";
 // import { useState } from "react";
 // import { PagerComponent } from "@syncfusion/ej2-react-grids";
 
-import { Header } from "components";
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const limit = 8;
+	const url = new URL(request.url);
+	const page = parseInt(url.searchParams.get("page") || "1", 10);
+	const offset = (page - 1) * limit;
 
-// export const loader = async ({ request }: LoaderFunctionArgs) => {
-// 	const limit = 8;
-// 	const url = new URL(request.url);
-// 	const page = parseInt(url.searchParams.get("page") || "1", 10);
-// 	const offset = (page - 1) * limit;
+	const { allTrips, total } = await getAllTrips(limit, offset);
 
-// 	const { allTrips, total } = await getAllTrips(limit, offset);
-
-// 	return {
-// 		trips: allTrips.map(({ $id, tripDetails, imageUrls }) => ({
-// 			id: $id,
-// 			...parseTripData(tripDetails),
-// 			imageUrls: imageUrls ?? [],
-// 		})),
-// 		total,
-// 	};
-// };
+	return {
+		trips: allTrips.map(({ $id, tripDetails, imageUrls }) => ({
+			id: $id,
+			...parseTripData(tripDetails),
+			imageUrls: imageUrls ?? [],
+		})),
+		total,
+	};
+};
 
 const Trips = ({ loaderData }: Route.ComponentProps) => {
-	// const trips = loaderData.trips as Trip[] | [];
+	const trips = loaderData.trips as Trip[] | [];
 
-	// const [searchParams] = useSearchParams();
-	// const initialPage = Number(searchParams.get("page") || "1");
+	const [searchParams] = useSearchParams();
+	const initialPage = Number(searchParams.get("page") || "1");
 
-	// const [currentPage, setCurrentPage] = useState(initialPage);
+	const [currentPage, setCurrentPage] = useState(initialPage);
 
-	// const handlePageChange = (page: number) => {
-	// 	setCurrentPage(page);
-	// 	window.location.search = `?page=${page}`;
-	// };
+	const handlePageChange = (page: number) => {
+		setCurrentPage(page);
+		window.location.search = `?page=${page}`;
+	};
 
 	return (
 		<main className="all-users wrapper">
@@ -53,7 +53,7 @@ const Trips = ({ loaderData }: Route.ComponentProps) => {
 					Manage Created Trips
 				</h1>
 
-				{/* <div className="trip-grid mb-4">
+				<div className="trip-grid mb-4">
 					{trips.map((trip) => (
 						<TripCard
 							key={trip.id}
@@ -73,7 +73,7 @@ const Trips = ({ loaderData }: Route.ComponentProps) => {
 					currentPage={currentPage}
 					click={(args) => handlePageChange(args.currentPage)}
 					cssClass="!mb-4"
-				/> */}
+				/>
 			</section>
 		</main>
 	);
